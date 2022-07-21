01bc6e541ee1c12ca95be6773b7f21b139454893
Nisarg Barot <barotnisarg83@gmail.com>
2022-07-21 22:48:05 +0530

    Initial Commit

---
+++ app/src/main/java/com/technic/loginui/CalorieCalculator.java
@@ -0,0 +1,44 @@
+package com.technic.loginui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class CalorieCalculator extends AppCompatActivity {
+
+    EditText weight,height,age;
+    Button calculate;
+    TextView result;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.caloriecalculator);
+        weight = findViewById(R.id.we);
+        height = findViewById(R.id.he);
+        age = findViewById(R.id.ag);
+        result = findViewById(R.id.finalanswer);
+        calculate = findViewById(R.id.calculate);
+        calculate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openNewActivity();
+            }
+        });
+
+
+    }
+    public void openNewActivity(){
+        double Weight, Age, Height, ResultM, ResultF;
+        Weight = Double.parseDouble(weight.getText().toString());
+        Age = Double.parseDouble(age.getText().toString());
+        Height = Double.parseDouble(age.getText().toString());
+        ResultM = (Weight * 100) + (Height * 6.25) - (Age * 5) + 5;
+        result.setText(String.valueOf(ResultM));
+
+    }
+}
---
+++ app/src/main/java/com/technic/loginui/healthTracker.java
@@ -0,0 +1,56 @@
+package com.technic.loginui;
+
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+
+public class healthTracker extends AppCompatActivity {
+    EditText weight,height,age;
+    TextView result;
+    RadioGroup radioButton;
+    RadioButton male,female;
+    Button button;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.healthtracker);
+        result= findViewById(R.id.Answer);
+        weight=findViewById(R.id.Weight);
+        height=findViewById(R.id.Height);
+        age= findViewById(R.id.Age);
+        male= findViewById(R.id.rb1);
+        female= findViewById(R.id.rb2);
+        button= findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                double w,h,r,a;
+                w=Double.parseDouble(weight.getText().toString());
+                h=Double.parseDouble(height.getText().toString());
+                a=Double.parseDouble(age.getText().toString());
+
+                switch (radioButton.getCheckedRadioButtonId()) {
+                    case R.id.rb1:
+                        r = 10 * w + 6.25 * h - 5 * a + 5;
+                        result.setText(String.valueOf(r));
+                        break;
+                    case R.id.rb2:
+                        r = 10 * w + 6.25 * h - 5 * a - 161;
+                        result.setText(String.valueOf(r));
+                        break;
+
+                }
+
+
+            }
+        });
+    }
+}
